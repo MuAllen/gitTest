1cde04b4a7ed332450f952190110f9529248a45d
Allen Mu <3371578364@qq.com>
2016-05-31 16:44:25 +0800

    创建工程

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by 久途 on 16/5/31.
+//  Copyright © 2016年 jiutu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by 久途 on 16/5/31.
+//  Copyright © 2016年 jiutu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
